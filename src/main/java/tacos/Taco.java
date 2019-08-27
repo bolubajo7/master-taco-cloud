56d8d0fb0afae3cb8478f1231703ae9526c6ad9b
boluwatife.olubajo <boluwatife.olubajo@influentialsoftware.com>
2019-08-27 14:16:49 +0100

    Added a process design to handle the post mapping.

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,6 +2,8 @@ package tacos;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -10,10 +12,13 @@ import java.util.List;
 public class Taco {
 
     private Long id;
-    private String name;
-
     private Date createdAt;
 
+    @NotNull
+    @Size(min=5, message ="Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients = new ArrayList<>();
 
     void createdAt() {
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,12 +3,16 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,6 +55,19 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
+        //Save the taco design
+        //We'll do this in chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 
 
