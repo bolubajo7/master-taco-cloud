175c90bd79065c4dcca813dae173802ca541b0e5
boluwatife.olubajo <boluwatife.olubajo@influentialsoftware.com>
2019-09-02 15:32:17 +0100

    Fixed a bug in the taco class.

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -62,10 +62,10 @@ public class DesignTacoController {
 
     @PostMapping
     public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
-        //if(errors.hasErrors()) {
-            //log.info("We have errors!");
-            //return "design";
-        //}
+        if(errors.hasErrors()) {
+            log.info("We have errors!");
+            return "design";
+        }
 
         //Save the taco design
         //We'll do this in chapter 3
